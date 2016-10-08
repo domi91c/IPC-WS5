84c460f0570ed550e1c6fed6ef27fbfc6487b6a4
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2016-10-08 07:48:21 -0400

    Workshop 5 student release

---
+++ in_lab/shopping_lab.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+
+
+int main(){
+	return 0;
+}
+
+/*
+Welcome to the Shop
+===================
+Please select from the following options:
+1) Display the inventory.
+2) Add to shop.
+0) Exit.
+Select:8
+Invalid input, try again: Please select from the following options:
+1) Display the inventory.
+2) Add to shop.
+0) Exit.
+Select:2
+Please input a SKU number:2341
+Quantity:3
+Price:12.78
+The item is successfully added to the inventory.
+Please select from the following options:
+1) Display the inventory.
+2) Add to shop.
+0) Exit.
+Select:2
+Please input a SKU number:4567
+Quantity:9
+Price:98.2
+The item is successfully added to the inventory.
+Please select from the following options:
+1) Display the inventory.
+2) Add to shop.
+0) Exit.
+Select:1
+
+
+Inventory
+=========================================
+Sku         Price       Quanity
+2341        12.78       3
+4567        98.20       9
+=========================================
+Please select from the following options:
+1) Display the inventory.
+2) Add to shop.
+0) Exit.
+Select:2
+Please input a SKU number:2341
+Quantity:5
+The item exists in the repository, quanity is updated.
+Please select from the following options:
+1) Display the inventory.
+2) Add to shop.
+0) Exit.
+Select:1
+
+
+Inventory
+=========================================
+Sku         Price       Quanity
+2341        12.78       8
+4567        98.20       9
+=========================================
+Please select from the following options:
+1) Display the inventory.
+2) Add to shop.
+0) Exit.
+Select:0
+Good bye!
+*/
